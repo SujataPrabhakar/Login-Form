301773d2482a659a36e0795c1e6475faf95539bb
Sujata Prabhakar <sujata_prabhakar@yahoo.com>
2023-09-05 19:19:11 +0400

    Sign Up Form

---
+++ LoginFormWithSqlServerDemo/FrmSignUp.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace LoginFormWithSqlServerDemo
+{
+    partial class FrmSignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblConfirmPassword = new System.Windows.Forms.Label();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(118, 115);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(60, 13);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "User Name";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(297, 115);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(118, 161);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(297, 158);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // lblConfirmPassword
+            // 
+            this.lblConfirmPassword.AutoSize = true;
+            this.lblConfirmPassword.Location = new System.Drawing.Point(118, 214);
+            this.lblConfirmPassword.Name = "lblConfirmPassword";
+            this.lblConfirmPassword.Size = new System.Drawing.Size(91, 13);
+            this.lblConfirmPassword.TabIndex = 4;
+            this.lblConfirmPassword.Text = "Confirm Password";
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Location = new System.Drawing.Point(297, 211);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtConfirmPassword.TabIndex = 5;
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.Location = new System.Drawing.Point(226, 297);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(75, 23);
+            this.btnSignUp.TabIndex = 6;
+            this.btnSignUp.Text = "Sign Up";
+            this.btnSignUp.UseVisualStyleBackColor = true;
+            this.btnSignUp.Click += new System.EventHandler(this.btnSignUp_Click);
+            // 
+            // FrmSignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 393);
+            this.Controls.Add(this.btnSignUp);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.lblConfirmPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblUserName);
+            this.Name = "FrmSignUp";
+            this.Text = "Sign Up";
+            this.Load += new System.EventHandler(this.FrmSignUp_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblConfirmPassword;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.Button btnSignUp;
+    }
+}
---
+++ LoginFormWithSqlServerDemo/FrmSignUp.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginFormWithSqlServerDemo
+{
+    public partial class FrmSignUp : Form
+    {
+        public FrmSignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmSignUp_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=LAPTOP-77PFC770;Initial Catalog=LoginUsers;Integrated Security=True";
+            con.Open();
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == txtConfirmPassword.Text)
+            {
+                using (SqlConnection connection = new SqlConnection())
+                {
+                    connection.ConnectionString = "Data Source=LAPTOP-77PFC770;Initial Catalog=LoginUsers;Integrated Security=True";
+                    connection.Open();
+
+                    string command = $"insert into LoginItems (UserName,UserPassword) values(@userName, @userPassword)";
+                    
+                    SqlCommand cmd = new SqlCommand(command, connection);
+                    cmd.Parameters.AddWithValue("@userName", txtUserName.Text);
+                    cmd.Parameters.AddWithValue("@userPassword", txtPassword.Text);
+
+                    int rowAffected = cmd.ExecuteNonQuery();
+                    if(rowAffected>0)
+                    {
+                        MessageBox.Show("Your Data added to the system");
+                        txtUserName.Text = " ";
+                        txtPassword.Text = " ";
+                        txtConfirmPassword.Text = " ";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Your Data not Added successfully. Please try again.");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Password and Confirm Password are not same!");
+            }
+
+
+        }
+
+       
+    }
+}
